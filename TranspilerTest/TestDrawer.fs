84e3755a8450988cdf973e25ec2f47dbcca6c934
Thyge Skødt Steffensen <s175176@dtu.dk>
2023-07-01 16:14:10 +0200

    test: Add test for drawer

---
+++ TranspilerTest/TestDrawer.fs
@@ -0,0 +1,38 @@
+﻿
+module TranspilerTest.AstDrawer
+
+open NUnit.Framework
+open FSharp.Text.Lexing
+open AstDrawerLibrary.Library
+open Transpiler
+open Transpiler.AstDrawer
+
+open TranspilerTest.Common
+
+[<SetUp>]
+let setup () = ()
+
+
+let dp =
+    { pos_bol = 1
+      pos_cnum = 1
+      pos_orig_lnum = 1
+      pos_fname =  "" 
+      pos_lnum = 1 }
+
+[<Test>]
+let TestType () =
+    let input = "Samples/NamedTransitionRulesSimple.rsl"
+    let actual =testLexerAndParserFromFile input
+    
+    let tree =
+        match actual.Value with
+        | (s, _pos), declarations -> Node($"Scheme {s}", List.foldBack declarationToNode declarations [])
+        
+    let postscriptActual = design tree |> toPSfast
+    
+    use textReader = new System.IO.StreamReader("Samples/NamedTransitionRulesSimple.ps")
+    let postscriptExpected = textReader.ReadToEnd ()
+    
+    Assert.AreEqual(postscriptActual, postscriptExpected)
+   
