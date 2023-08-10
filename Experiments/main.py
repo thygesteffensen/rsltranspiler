4b93b1f0c133b4f42efe2e02391a3321680c1773
Thyge Skødt Steffensen <s175176@dtu.dk>
2023-08-10 11:29:07 +0200

    update script

--- Experiments/main.py
+++ Experiments/main.py
@@ -1,20 +1,36 @@
 import glob
+
 import pandas
+import numpy
+
 
 def meaner(tool: str):
     csv_files = glob.glob(f'{tool}_*.csv')
 
-    df_collection = pandas.read_csv(csv_files[0], sep=',', header=0, names=['n', 'real', 'user', 'sys', 'mRSS'])
+    n = []
+    time = [[]]
+    mrss = [[]]
 
-    for csv_file in csv_files[1:]:
+    for csv_file in csv_files:
         print(csv_file)
         df = pandas.read_csv(csv_file, sep=',', header=0, names=['n', 'real', 'user', 'sys', 'mRSS'])
-        df_collection += df
 
-    df_collection = df_collection / 11
-    df_collection.to_csv(f"mean_{tool}.csv", sep=',', index=False)
+        for i, row in df.iterrows():
+            if (i + 1) > len(n):
+                n.append(0)
+                time.append([])
+                mrss.append([])
+            n[i] = int(row['n'])
+            time[i].append(row['real'])
+            mrss[i].append(row['mRSS'])
+
+    with open(f'mean_{tool}.csv', "w") as f:
+        print('n,time_mean,time_std_err,mrss_mean,mrss_std_err', file=f)
+        for (n, t, m) in zip(n, time, mrss):
+            print(f'{n},{numpy.mean(t)},{numpy.std(t)},{numpy.mean(m)},{numpy.std(m)}', file=f)
 
     print("Done")
+
 
 meaner('rslts_t')
 meaner('rslts_s')
